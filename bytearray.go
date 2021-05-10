7957c88dcf7272ce1fb4a43be2011032b268ce95
John <johnfg2610@gmail.com>
2021-05-10 21:39:43 +0100

    init

---
+++ bytearray.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+type ByteArray []byte
+
+func (ba *ByteArray) UnmarshalGQL(v interface{}) error {
+	arr, ok := v.([]byte)
+	if !ok {
+		return fmt.Errorf("ByteArray must be a array of bytes")
+	}
+
+	*ba = arr
+	return nil
+}
+
+// MarshalGQL implements the graphql.Marshaler interface
+func (ba ByteArray) MarshalGQL(w io.Writer) {
+	w.Write(ba)
+}
